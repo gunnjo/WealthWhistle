5577c3e79edf9ea0d6d276d10207db1a7ae95904
Dry Bones <telrikk@gmail.com>
2012-12-01 20:44:08 -0500

    Rails app bundled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WealthWhistle::Application.config.secret_token = '963bc6d44acf04c0714e8c9f840b1f99b5ec02b28e3829730b22df048e922912540d43f72ebb38493d41def0ed91d291cfdf0643d5a0e37390d9476244297e5a'
